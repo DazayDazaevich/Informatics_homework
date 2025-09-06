15c3eb6309cfff6ac5c9186ee9ea6ff3643e4c8b
DazayDazaevich <mihaska2006@gmail.com>
2025-09-06 19:45:12 +0300

    Add files via upload

---
+++ lib.cpp
@@ -0,0 +1,8 @@
+namespace my_lib
+{
+int foo(int x)
+{return x*3;}
+
+void main();
+
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+}
+//rm -r build/
+//cmake -B build
